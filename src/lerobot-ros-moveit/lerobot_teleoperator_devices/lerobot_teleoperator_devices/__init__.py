86af00ed4233bee2dd77349285b1beb61042605a
Vsadnik <mur-agent@yandex.ru>
2026-06-14 13:20:39 +0300

    Removing useless nodes

--- src/lerobot-ros-moveit/lerobot_teleoperator_devices/lerobot_teleoperator_devices/__init__.py
+++
@@ -1,9 +0,0 @@
-from .config_keyboard_joint import KeyboardJointTeleopConfig, KeyboardJointHardwareTeleopConfig
-from .keyboard_joint import KeyboardJointTeleop, KeyboardJointHardwareTeleop
-
-# Optional gamepad support (requires pygame)
-try:
-    from .config_gamepad_6dof import Gamepad6DOFTeleopConfig
-    from .gamepad_6dof import Gamepad6DOFTeleop
-except ImportError:
-    pass
